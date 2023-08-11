f7d11de7dc885e6649d581a45ea601e8f92854a5
falsofacu <facundotabarez0@gmail.com>
2023-08-11 19:52:29 -0300

    fix: button running div onclick event

--- evento.js
+++ evento.js
@@ -1,5 +1,11 @@
 const divElement = document.getElementsByTagName("div")[0];
+const buttonElement = document.getElementsByTagName("button")[0];
 
 divElement.addEventListener("click", () => {
   alert("Hola! Soy el div");
+})
+
+buttonElement.addEventListener("click", (event) => {
+  event.stopPropagation();
+  alert("Hola!");
 })
